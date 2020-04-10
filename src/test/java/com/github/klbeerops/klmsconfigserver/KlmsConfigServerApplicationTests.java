96e8fc22c7ef92a411e32cf65fa85d93775033f0
Rob <rjboyce@teksavvy.com>
2020-04-10 18:10:56 -0400

    Initial project with example Github configuration.  Closes #1

---
+++ src/test/java/com/github/klbeerops/klmsconfigserver/KlmsConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.klbeerops.klmsconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlmsConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
